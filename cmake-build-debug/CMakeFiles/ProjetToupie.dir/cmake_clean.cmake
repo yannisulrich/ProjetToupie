6befd38b40dc0ca91dc64c29ddc66298f2d6baf8
Yannis Ulrich <yannis.ulrich2@gmail.com>
2020-02-27 10:54:09 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProjetToupie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetToupie.dir/main.cpp.o"
+  "ProjetToupie"
+  "ProjetToupie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetToupie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
